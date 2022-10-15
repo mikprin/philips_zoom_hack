b0653fae4cfa9113ec6ef01d4957c294bf0324d0
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2022-10-15 20:19:45 +0400

    scout found

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 
 
+# Structure
+
+The project is structured as follows:
+
+        `src/` contains the source code of the project
+           |- `onewire_scout` contains my platformio project for the ESP32 onewire scout
+           |- `onewire_example_code` contains example code for the OneWire library
+
+# Scout
+
+`R=AD C6 43 AC 0 20 0 B0`
+
 # Useful links
 chip: https://www.digikey.com/en/products/detail/analog-devices-inc-maxim-integrated/DS28E05P/3877429
 onewire: https://www.maximintegrated.com/en/design/technical-documents/tutorials/1/1796.html
--- esp32_src/onewire_scout/src/main.cpp
+++ esp32_src/onewire_scout/src/main.cpp
@@ -5,6 +5,16 @@
 #include "pin_defines.h"
 
 
+// RTOS includes
+extern "C"
+{
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+#include "freertos/task.h"
+}
+
+static SemaphoreHandle_t one_wire_read_mutex;
+
 OneWire  ds(ONE_WIRE_PIN);  // on pin ONE_WIRE_PIN
 
 void setup() {
@@ -19,5 +29,37 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Loop started");
+  uint i;
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1);         // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+
+  if ( !ds.search(addr)) {
+    Serial.print("No more addresses.\n");
+    ds.reset_search();
+    vTaskDelay(250 / portTICK_PERIOD_MS);
+    return;
+  }
+
+  Serial.print("R=");
+  for( i = 0; i < 8; i++) {
+    Serial.print(addr[i], HEX);
+    Serial.print(" ");
+  }
+
+  // Read onewire memory
+
+  present = ds.reset();
+  ds.select(addr);
+  
   
 }
